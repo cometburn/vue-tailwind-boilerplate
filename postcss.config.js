2d8a44c2e256e9c653e7946b1a76034c328edfc3
Jc Cometa <cometburn@gmail.com>
2021-03-05 15:38:31 +0800

    Init

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "plugins": [
+    require('tailwindcss')('tailwind.config.js'),
+    require("autoprefixer")
+  ]
+}
+
+// module.exports = { 
+//   plugins: [ 
+//       // ... 
+//       require("tailwindcss"),
+//       require("autoprefixer"), 
+//       // ... 
+//   ],
+// }
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="md:container md:mx-auto">
+    <div class="grid grid-cols-3 gap-2">
+      <div>1</div>
+      <div>9</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
