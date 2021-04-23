bc9a1059ab7d1ade1cf9d0707362deea6e4cd18f
Jc Cometa <cometburn@gmail.com>
2021-04-23 19:05:08 +0800

    Add eslint and fix syntax

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
 module.exports = {
+  publicPath: '/',
   devServer: {
-    port: 7000
-  }
+    host: 'localhost',
+    port: 6999,
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: '@import "~@/assets/styles/variables.scss";',
+      },
+    },
+  },
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js',
+      exclude: [
+        /\.map$/,
+        /manifest\.json$/,
+      ],
+    },
+    themeColor: '#1da025',
+  },
 }
